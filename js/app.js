114735716531541ddecd9173d735a8f5a31306e7
Shaun <scpour@gmail.com>
2023-07-10 11:24:01 -0400

    Setup basic js to write time difference to the page

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
 const btn = document.querySelector('.arrow');
 const currentDate = new Date();
+const yearOut = document.querySelector('.years span');
+const monthOut = document.querySelector('.months span');
+const dayOut = document.querySelector('.days span');
 
 btn.addEventListener('click', () => {
-    let day = document.querySelector('.input-day').value;
-    let month = document.querySelector('.input-month').value;
-    let year = document.querySelector('.input-year').value;
-    let DateVal = new Date(`${month} ${day} ${year}`);
-    console.log(DateVal-currentDate);
+
+    let day = document
+        .querySelector('.input-day')
+        .value;
+    let month = document
+        .querySelector('.input-month')
+        .value;
+    let year = document
+        .querySelector('.input-year')
+        .value;
+
+    let birthday = `${month}/${day}/${year}`;
+    let birthdayObj = new Date(birthday);
+    let ageDiff = Date.now() - birthdayObj;
+    let ageDate = new Date(ageDiff);
+    let ageYears = ageDate.getUTCFullYear() - 1970;
+    let ageMonths = ageDate.getUTCMonth();
+    let ageDays = ageDate.getUTCDay() - 1;
+
+    yearOut.textContent = ageYears;
+    monthOut.textContent = ageMonths;
+    dayOut.textContent = ageDays;
 });
